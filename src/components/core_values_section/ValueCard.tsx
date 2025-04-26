e900d59fdf2be04a2ac1dfb72f2dadbfba009fbf
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-04-26 07:05:20 +0300

    Make CoreValues Cards Splitted Colors.

--- src/components/core_values_section/ValueCard.tsx
+++ src/components/core_values_section/ValueCard.tsx
@@ -1,9 +1,9 @@
 function ValueCard({ title, description, illustration }: { title: string; description: string; illustration?: string }) {
   return (
-    <div className={`hover:bg-secondary duration-fast group flex items-center gap-2 rounded-3xl border border-b-4 p-8 hover:text-white max-lg:flex-col [&>*]:flex-1`}>
+    <div className={`[&:nth-child(3n+1)]:bg-secondary duration-fast group flex items-center gap-2 rounded-3xl border border-b-4 p-8 max-lg:flex-col [&:nth-child(3n+1)]:text-white [&>*]:flex-1`}>
       <div className="grid content-start gap-6">
         <h3 className="text-2xl font-medium">{title}</h3>
-        <hr className="border-secondary group-hover:border-gray" />
+        <hr className="" />
         <p>{description}</p>
       </div>
 
