5b9d44b418d70dbbdc8fa9955c9ff608604f662e
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-04-29 15:07:46 +0300

    Adding ScrollToTop functionality when pathname changes.

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/header/Index";
 import Footer from "../components/footer/Index";
+import { useEffect } from "react";
 
 function Layout() {
+  const { pathname } = useLocation();
+
+  // Scroll to top when the pathname changes.
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
+
   return (
     <>
       <Header />
