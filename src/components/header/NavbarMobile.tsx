0950369d8c461e9ef6913a7f186367a5f477d5c4
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-04-29 16:04:18 +0300

    Adding Border Top for Mobile Navbar.

--- src/components/header/NavbarMobile.tsx
+++ src/components/header/NavbarMobile.tsx
@@ -7,7 +7,7 @@ function NavbarMobile({ isOpened }: { isOpened: boolean }) {
   }
 
   return (
-    <div className="absolute top-full left-0 container w-full justify-center gap-4 border-b bg-white py-8 text-lg lg:hidden">
+    <div className="absolute top-full left-0 container w-full justify-center gap-4 border-t border-b bg-white py-8 text-lg lg:hidden">
       <nav>
         <ul className="grid auto-rows-[minmax(40px,1fr)] justify-center text-center">{navLinksHeaderElements}</ul>
       </nav>
