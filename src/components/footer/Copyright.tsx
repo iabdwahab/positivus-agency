c85f6d4647a4c6f3d5d27c7af69bd712a1474d9f
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-04-29 14:54:16 +0300

    Footer Hover Effect.

--- src/components/footer/Copyright.tsx
+++ src/components/footer/Copyright.tsx
@@ -2,7 +2,7 @@ function Copyright() {
   return (
     <div className="flex items-center gap-4 max-md:flex-col">
       <p>© 2023 Positivus. All Rights Reserved.</p>
-      <a href="#" className="border-b border-white">
+      <a href="#" className="border-b border-white hover:opacity-75">
         Privacy Policy
       </a>
     </div>
--- src/components/footer/NavLinks.tsx
+++ src/components/footer/NavLinks.tsx
@@ -4,29 +4,30 @@ import logoLight from "/global/logo-text-light.svg";
 import linkedinIcon from "/social/linkedin-light.svg";
 import facebookIcon from "/social/facebook-light.svg";
 import twitterIcon from "/social/twitter-light.svg";
+import { Link } from "react-router-dom";
 
 function NavLinks() {
   return (
     <div className="flex items-center justify-between gap-8 max-lg:flex-col">
-      <div>
+      <Link to="/">
         <img src={logoLight} alt="Logo" />
-      </div>
+      </Link>
 
       <ul className="flex items-center gap-4 max-lg:flex-col">{navLinksFooterElements}</ul>
 
       <ul className="flex items-center gap-4">
         <li>
-          <a href="#">
+          <a href="#" className="hover:opacity-80">
             <img src={linkedinIcon} alt="Linkedin Icon" />
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="#" className="hover:opacity-80">
             <img src={facebookIcon} alt="Linkedin Icon" />
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="#" className="hover:opacity-80">
             <img src={twitterIcon} alt="Linkedin Icon" />
           </a>
         </li>
